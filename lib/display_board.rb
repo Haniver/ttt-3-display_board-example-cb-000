69c98417aa088b66bcd6d020a7209ac7272be945
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-03-12 22:34:54 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def printBoard
+  normal = "   |   |   "
+  separator = "-----------"
+  puts normal
+  puts separator
+  puts normal
+  puts separator
+  puts normal
+end
